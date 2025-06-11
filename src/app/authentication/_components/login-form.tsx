654e3b3be42a540a302bcbe8dc722fc4c3a7a022
Renato Reboucas <renato190698@gmail.com>
2025-06-11 11:06:12 -0300

    feat: add logging for Spotify login process in login form

--- src/app/authentication/_components/login-form.tsx
+++ src/app/authentication/_components/login-form.tsx
@@ -68,6 +68,7 @@ export default function LoginForm() {
     });
   };
   const handleSpotifyLogin = async () => {
+    console.log("handleSpotifyLogin");
     await authClient.signIn.social({
       provider: "spotify",
       callbackURL: "/dashboard",
